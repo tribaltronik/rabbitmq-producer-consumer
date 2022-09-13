d0f42cb82d7b8f787e78bc1ed10f889c113340c8
Tiago Ricardo <ricardo@unicc.org>
2022-09-13 17:52:57 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# rabbitmq-producer-consumer
-RabbitMQ test with a producer and consumer
+# RabbitMQ Test with procucer and consumer in Go Lang
+
+
+## Docker run rabbitmq
+
+docker run -d --name rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+
+
+## Run producer
+
+go run main.go
+
+## Run consumer
+
+go run consumer.go
+
+## Access RabbitMQ UI
+
+http://localhost:15672
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("GO RabbitMQ")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully Connected To our RabbitMQ Instance")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Successfully Published Message to Queue")
+}
